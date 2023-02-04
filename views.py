22f2931841d5bb07c930ceae823076936bb3dfdd
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-02-04 17:43:44 -0300

    Subindo imagens para o servidor

--- views.py
+++ views.py
@@ -28,6 +28,9 @@ def criar():
     db.session.add(novo_jogo)
     db.session.commit()
     
+    arquivo = request.files.get('arquivo')
+    arquivo.save(f'uploads/{arquivo.filename}')
+
     return redirect(url_for('index'))
 
 @app.route('/editar/<int:id>')
